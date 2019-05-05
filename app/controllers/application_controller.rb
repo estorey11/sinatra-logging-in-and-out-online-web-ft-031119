0d9ee3bae02ad19945b9a11e6169423e0248f2de
Eric Storey <estorey@gmail.com>
2019-05-05 19:53:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,16 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user=User.find_by(username: params[:username], password: params[:password])
+    if @user 
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
   end
 
   get '/account' do
-
+    @user = User.find(session[:user_id])
+    erb :account
   end
 
   get '/logout' do
