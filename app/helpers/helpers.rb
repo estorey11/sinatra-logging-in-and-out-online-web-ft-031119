54f52ab21b03467c958ef7d0e1768369d5e74f72
Eric Storey <estorey@gmail.com>
2019-05-05 20:07:02 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+  
+  def current_user(session_hash)
+    User.find(session_hash[:user_id])
+  end
+  
+  def is_logged_in?(session_hash)
+    !!session_hash[:user_id]
+  end
+  
+  
 end
